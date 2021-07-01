af73fc902ef9522a4f1ea38f1b153410f35fb733
Space V <40030799+TheSpaceGamerV2@users.noreply.github.com>
2021-07-01 15:19:48 +0200

    Change _resource.lua to fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+ 
+ games { 'gta5' }
+ 
+ author 'stveu'
+ 
+ description 'Scripts allows players to search any dumpster in the game to acquire items that they find.'
+ 
+ version '1.0.0'
+ 
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
